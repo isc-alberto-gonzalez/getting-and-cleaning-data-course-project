3ff296acb1eb75a35a475ec5892baddf95b89da7
isc-alberto-gonzalez <isc.alberto.gonzalez@icloud.com>
2021-02-06 15:17:16 -0600

    INCLUDE CODE TO DOWNLOAD THE DATASET

--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,30 @@
+## 0) Downloaded the files
+
+if (!file.exists("data")) {dir.create("data")}
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+download.file(fileUrl, destfile = "./data/project.zip")
+
+# Unzip Data
+
+unzip(zipfile = "./data/project.zip", exdir = "./data")
+
+
 ## 1) Merges the training and the test sets to create one data set.
 
 ##read the files using read.table ; the data are loaded in dataframes
-subject_train <- read.table("./train/subject_train.txt")
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 
-y_train <- read.table("./train/y_train.txt")
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
 
-x_train <- read.table("./train/x_train.txt")
+x_train <- read.table("./data/UCI HAR Dataset/train/x_train.txt")
 
-subject_test <- read.table("./test/subject_test.txt")
+subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
 
-y_test <- read.table("./test/y_test.txt")
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
 
-x_test <- read.table("./test/x_test.txt")
+x_test <- read.table("./data/UCI HAR Dataset/test/x_test.txt")
 
 #rename the column 'V1' to 'subject' in dataframe 'subject_train'
 colnames(subject_train)[colnames(subject_train) == 'V1'] <- 'subject'
